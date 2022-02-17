95dd5698203f044d698eaa2c002822df96d21f8c
Eric Romano <ecr1980@gmail.com>
2022-02-17 11:38:28 -0500

    Added a few methods to count the number of times each value is guessed, and used. Changed the score method to only check for exact matches before checking for partial matches. That method is still being worked on, but I wanted to make a commit now to show the transistion from the previous version more clearly. This new setup should prevent the score from showing partial matches when the matching value has already been matched.
    In other words, if I guess all blue, but only the second value is blue, the scoreboard should
    indicate that I got [wrong, exact, wrong, wrong] instead of [partial, exact, partial, partial]

--- mastermind.rb
+++ mastermind.rb
@@ -18,36 +18,36 @@ end
 
 def game_setup
    $secret_code = comp_choice()
-   $turns = Array.new(12) {Array.new(4, "X ")}
+   $turns = Array.new(12) {Array.new(4, "-")}
    $turns_results = Array.new(12) {Array.new(4, " +")}
    $player = true
 end
 
 def game_display
   $turns.each_with_index do |turn, i|
-    puts " #{turn[0]} #{turn[1]} #{turn[2]} #{turn[3]}    #{$turns_results[i][0]} #{$turns_results[i][1]} #{$turns_results[i][2]} #{$turns_results[i][3]}"
+    puts " #{turn[0]}  #{turn[1]}  #{turn[2]}  #{turn[3]}     #{$turns_results[i][0]} #{$turns_results[i][1]} #{$turns_results[i][2]} #{$turns_results[i][3]}"
   end
 end
 
 def player_choice
-  ok = true
+  $OK = true
   puts "Please enter the four colors you want, in order."
   puts "Choices are: Blue, Yellow, Red, Orange, Green, and Purple"
   choice = gets.upcase.chomp
   choice = choice.split(" ")
   if choice.length != 4
-    "Only four choices, please."
-    ok = false
+    puts "Four choices, please."
+    $OK = false
   end
   choice.each do |color,index|
     color = color.chr
     if (color != "B" && color != "Y" && color != "R" && color != "O" && color != "G" && color != "P")
       puts "Only valid choices, please."
-      ok = false
+      $OK = false
       break
     end
   end
-  array_change[color]
+  choice
 end
 
 def comp_choice
@@ -76,12 +76,115 @@ def comp_choice
   return choice
 end
 
+def count(x)
+  color_count = Array.new(6,0)
+
+  x.each do |value|
+    case value
+    when "B"
+      color_count[0] += 1
+    when "Y"
+      color_count[1] += 1
+    when "R"
+      color_count[2] += 1
+    when "O"
+      color_count[3] += 1
+    when "G"
+      color_count[4] += 1
+    when "P"
+      color_count[5] += 1
+    end
+  end
+  color_count
+end
+
+def count_minus(x, value)
+    case value
+    when "B"
+      x[0] -= 1
+    when "Y"
+      x[1] -= 1
+    when "R"
+      x[2] -= 1
+    when "O"
+      x[3] -= 1
+    when "G"
+      x[4] -= 1
+    when "P"
+      x[5] -= 1
+    end
+  end
+  x
+end
+
+def color_array_location(x)
+  case x
+  when "B"
+    y = 0
+  when "Y"
+    y = 1
+  when "R"
+    y = 2
+  when "O"
+    y = 3
+  when "G"
+    y = 4
+  when "P"
+    y = 5
+  end
+end
+
+
+
+def score_check(turn, x)
+  color_count = count(turn)
+  turn.each_with_index do |value, i|
+    if value == $secret_code[i]
+      $turns_results[x][i] = " 2"
+      color_count = count_minus(color_count, value)
+    end
+  end
+  turn.each_with_index do |value, i|
+    c_a_l = color_array_location(value)
+    $secret_code.each_with_index do |secret, j|
+      if secret == value
+        $turns_results[x][i] = " 1"
+        break
+      else
+        $turns_results[x][i] = " 0"
+      end
+    end
+  end     
+  if turn != $secret_code
+    return false
+  else
+    return true
+  end
+end
+
+
+def game_loop()
+  $turns.each_with_index do |turn, i|
+    $OK = false
+    while $OK == false
+      turn = player_choice()
+    end
+    $turns[i] = turn
+    win = score_check(turn, i)
+    game_display()
+    if win
+      puts "You win!"
+      break
+    end
+  end
+end
+    
+
 
 game_setup()
 
 game_display()
 
-#YOU ARE OVER THINKING THIS. YOU DON'T NEED TO STORE YOUR GUESSES IN A CLASS
-#VARIABLE! YOU CAN JUST HAVE AN ARRAY OF STORED VALUES.
+game_loop()
 
 
