47e0ca6be04b42ea4019154f888e5818b4b9919e
Eric Romano <ecr1980@gmail.com>
2022-02-16 22:52:55 -0500

    Now contains a method for the players choice, as well as a basic method for the computer's choice. The computer's choice does not yet contain logic for reacting to previous turns.

--- mastermind.rb
+++ mastermind.rb
@@ -47,6 +47,53 @@ def game_display
   $choice_12.array_display()
 end
 
+def player_choice
+  ok = true
+  puts "Please enter the four colors you want, in order."
+  puts "Choices are: Blue, Yellow, Red, Orange, Green, and Purple"
+  choice = gets.upcase.chomp
+  choice = choice.split(" ")
+  if choice.length != 4
+    "Only four choices, please."
+    ok = false
+  end
+  choice.each do |color,index|
+    color = color.chr
+    if (color != "B" && color != "Y" && color != "R" && color != "O" && color != "G" && color != "P")
+      puts "Only valid choices, please."
+      ok = false
+      break
+    end
+  end
+  array_change(color)
+end
+
+def comp_choice
+  choice = Array.new(4)
+
+  choice.each_index do |choice,index|
+    x = rand(6)
+    case x
+    when 0
+      x = "B"
+    when 1
+      x = "Y"
+    when 2
+      x = "R"
+    when 3
+      x = "O"
+    when 4
+      x = "G"
+    when 5
+      x = "P"
+    else
+      puts "Generic Error, what gives?"
+    end
+    choice[index] = x
+  end
+  return choice
+end
+
 game_setup()
 
 game_display()
